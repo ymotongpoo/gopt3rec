f741b2d6e6247c9881241349792eb93dfb0bc309
Yoshi Yamaguchi <ymotongpoo@gmail.com>
2015-06-06 07:27:54 +0900

    fixed channel mapping

--- command/helper.go
+++ command/helper.go
@@ -43,7 +43,7 @@ var TVChannelMap = map[string]string{
 	"mx":     "16",
 	"12":     "28",
 	"univ":   "28",
-	"nhkbs1": "BS15_1",
+	"nhkbs1": "BS15_0",
 	"nhkbs2": "BS15_1",
 	"bsntv":  "BS13_0",
 	"bsex":   "BS01_0",
